34920cbc93964a2180ba4a4152245bd429cd19a3
Fahrirahmat-06 <eterletda915@gmail.com>
2025-08-11 18:22:31 +0700

    Add files via upload

---
+++ order.js
@@ -0,0 +1,44 @@
+const Order = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Panding'
+    },
+    {
+        productName: 'LARVENDER KF102 Drone',
+        productNumber: '36378',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Ruko F11 Pro Drone',
+        productNumber: '49347',
+        productStatus: 'Due',
+        shipping: 'Panding'
+    },
+    {
+        productName: 'Drone with Camera Drone',
+        productNumber: '96996',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'GPS 4k Drone',
+        productNumber: '22821',
+        paymentStatus: 'Paid',
+        shipping: 'Deliversed'
+    },
+    {
+        productName: 'DJI Air 25',
+        productNumber: '81475',
+        paymentStatus: 'Due',
+        shipping: 'Panding'
+    },
+    {
+        productName: 'Lozenge Drone',
+        productNumber: '00482',
+        paymentStatus: 'Paid',
+        shipping: 'Deliversed'
+    }
+]
